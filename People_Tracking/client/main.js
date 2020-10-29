1d13d417ea8c5c9d6301f4ec016851f0ea6455ce
Abdullah-Alrefaey <abdullah.m.alrefaey@gmail.com>
2020-10-29 16:59:05 +0200

    Added map and move functions

--- People_Tracking/client/main.js
+++ People_Tracking/client/main.js
@@ -3,20 +3,54 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+const room_map = {
+  '00': { top: '55%', left: '50%'},
+  '01': { top: '20%', left: '50%'},
+  1: { top: '64%', left: '80%'},
+  2: { top: '50%', left: '80%'},
+  3: { top: '36%', left: '80%'},
+  4: { top: '22%', left: '80%'},
+  5: { top: '8%', left: '80%'},
+  6: { top: '64%', left: '20%'},
+  7: { top: '50%', left: '20%'},
+  8: { top: '36%', left: '20%'},
+  9: { top: '22%', left: '20%'},
+  10: { top: '8%', left: '20%'},
+}
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
+Template.body.events({
+  'submit .new-move'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+
+    // Get value from form element
+    const target = event.target;
+    const number = target.room_number.value;
+
+    $(".human").css({
+        top: room_map[number]['top'],
+        left: room_map[number]['left']
+    });
+
+    // Clear form
+    target.room_number.value = '';
   },
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+// Template.hello.onCreated(function helloOnCreated() {
+//   // counter starts at 0
+//   this.counter = new ReactiveVar(0);
+// });
+//
+// Template.hello.helpers({
+//   counter() {
+//     return Template.instance().counter.get();
+//   },
+// });
+//
+// Template.hello.events({
+//   'click button'(event, instance) {
+//     // increment the counter when button is clicked
+//     instance.counter.set(instance.counter.get() + 1);
+//   },
+// });
