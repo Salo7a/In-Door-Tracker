f2281a090f18e170653c1bcca2b9b706edb054b3
Abdullah-Alrefaey <abdullah.m.alrefaey@gmail.com>
2020-10-31 19:23:05 +0200

    Fix mapping

--- People_Tracking/client/main.js
+++ People_Tracking/client/main.js
@@ -25,18 +25,17 @@ firebase.initializeApp(firebaseConfig);
 let scanned_networks = { }
 
 const room_map = {
-  '00': { top: '55%', left: '50%'},
-  '01': { top: '20%', left: '50%'},
-  1: { top: '64%', left: '80%'},
-  2: { top: '50%', left: '80%'},
-  3: { top: '36%', left: '80%'},
-  4: { top: '22%', left: '80%'},
-  5: { top: '8%', left: '80%'},
-  6: { top: '64%', left: '20%'},
-  7: { top: '50%', left: '20%'},
-  8: { top: '36%', left: '20%'},
-  9: { top: '22%', left: '20%'},
-  10: { top: '8%', left: '20%'},
+  '00': { top: '60%', left: '50%'},
+  '01': { top: '43%', left: '50%'},
+  '02': { top: '23%', left: '50%'},
+  1: { top: '62%', left: '80%'},
+  2: { top: '45%', left: '80%'},
+  3: { top: '28%', left: '80%'},
+  4: { top: '10%', left: '80%'},
+  5: { top: '62%', left: '20%'},
+  6: { top: '45%', left: '20%'},
+  7: { top: '28%', left: '20%'},
+  8: { top: '10%', left: '20%'},
 }
 
 Template.body.events({
@@ -75,20 +74,3 @@ Template.body.events({
   },
 });
 
-// Template.hello.onCreated(function helloOnCreated() {
-//   // counter starts at 0
-//   this.counter = new ReactiveVar(0);
-// });
-//
-// Template.hello.helpers({
-//   counter() {
-//     return Template.instance().counter.get();
-//   },
-// });
-//
-// Template.hello.events({
-//   'click button'(event, instance) {
-//     // increment the counter when button is clicked
-//     instance.counter.set(instance.counter.get() + 1);
-//   },
-// });
