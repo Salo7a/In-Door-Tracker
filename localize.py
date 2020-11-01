d81679b6a3bd50e3af00f202757363dfaa0ec913
Ahmad <ahmedmageed19@gmail.com>
2020-11-01 22:10:14 +0200

    loading model from js

--- localize.py
+++ localize.py
@@ -1,5 +1,18 @@
-from joblib import load
+# Simple loader script, loads the saved model from the notebook
+# and predict the class from the choosen classifier 
 
-filename = "model.joblib"
+from joblib import load
+import sys 
+
+
+# model file name 
+filename = sys.argv[1]
+
+# Load classifier
 clf = load(filename)
 
+# Get the strengths values 
+vals = [float(i) for i in sys.argv[2:]]
+
+# Print predicted class to console 
+print(clf.predict([vals]))
