bdac719ea2a785eab982b84a44d0880394285b3c
Ahmad <ahmedmageed19@gmail.com>
2020-11-02 13:51:23 +0200

    added last rooms, fit final model and save

--- People_Tracking/server/main.js
+++ People_Tracking/server/main.js
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 const {spawn} = require('child_process');
-const python = spawn('python', ['localize.py', 'model.joblib', '-76.', '-58.', '-88.', '-86.', '-87.1', '-89.', '-87.', '-74.', '-76.6']);
+const python = spawn('python', ['./localize.py', 'model.joblib', '-76.', '-58.', '-88.', '-86.', '-87.1', '-89.', '-87.', '-74.', '-76.6']);
 
 console.log('ff');
 // Event waiting for python console to print location 
--- localize.js
+++ localize.js
@@ -1,5 +1,5 @@
 const {spawn} = require('child_process');
-const python = spawn('python', ['localize.py', 'model.joblib', ['-76.', '-58.', '-88.', '-86.', '-87.1', '-89.', '-87.', '-74.', '-76.6']])
+const python = spawn('python', ['localize.py', 'model.joblib', '-76.', '-58.', '-88.', '-86.', '-87.1', '-89.', '-87.', '-74.', '-76.6']);
 let location ;
 
 // Event waiting for python console to print location 
